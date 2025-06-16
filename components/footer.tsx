be8505a3825073523c5ac5688fd544e9ba05e826
Mufeed Rahman <mufeed.rahman@legendholding.com>
2025-06-16 10:31:55 +0400

    footer-update

--- components/footer.tsx
+++ components/footer.tsx
@@ -69,19 +69,6 @@ const YoutubeIcon = ({ size = 20, className = "" }) => (
   </svg>
 )
 
-const TikTokIcon = ({ size = 20, className = "" }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className={className}
-  >
-    <path d="M19.59 6.69a4.83 4.83 0 01-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 01-5.2 1.74 2.89 2.89 0 012.31-4.64 2.93 2.93 0 01.88.13V9.4a6.84 6.84 0 00-1-.05A6.33 6.33 0 005 20.1a6.34 6.34 0 0010.86-4.43v-7a8.16 8.16 0 004.77 1.52v-3.4a4.85 4.85 0 01-1-.1z" />
-  </svg>
-)
-
 export function Footer() {
   const currentYear = new Date().getFullYear()
 
@@ -263,14 +250,6 @@ export function Footer() {
                 className="text-white/80 hover:text-white transition-all duration-300"
               >
                 <YoutubeIcon size={20} />
-              </Link>
-              <Link
-                href="https://tiktok.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white/80 hover:text-white transition-all duration-300"
-              >
-                <TikTokIcon size={20} />
               </Link>
             </div>
             {/* <p className="text-white/60 text-xs">
